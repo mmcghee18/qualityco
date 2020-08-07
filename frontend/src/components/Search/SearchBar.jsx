bb5c86251d948777d41623491c05a7d52c79bab0
Michelle <michmac202@gmail.com>
2020-08-07 15:18:52 -0400

    dropdown for search bar

--- frontend/src/components/Search/SearchBar.jsx
+++ frontend/src/components/Search/SearchBar.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
 import { Redirect } from "react-router-dom";
-import { Input } from "antd";
+import { Input, Select } from "antd";
 const { Search } = Input;
+const { Option } = Select;
 
 const SearchBar = () => {
   const [showResults, setShowResults] = useState(false);
   const [searchTerm, setSearchTerm] = useState(null);
+  const [type, setType] = useState("products");
+
+  const selectBefore = (
+    <Select
+      defaultValue="Products"
+      className="select-before"
+      onChange={(value) => setType(value.toLowerCase())}
+    >
+      <Option value="Products">Products</Option>
+      <Option value="Services">Services</Option>
+    </Select>
+  );
 
   if (showResults) {
-    //return <Redirect to={"/search"} />;
-    return <Redirect to={`/search?q=${searchTerm}`} />;
+    return <Redirect to={`/search?type=${type}&q=${searchTerm}`} />;
   }
   return (
     <Search
+      addonBefore={selectBefore}
       placeholder="I'm looking for..."
       onSearch={(value) => {
         setSearchTerm(value);
